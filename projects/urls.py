1cdf55dd5b1c08e0f271f28164aa84421781c657
Eka Putra <eka@insight.co>
2021-02-13 22:28:47 +1100

    feat(projects): #5 added page for project user

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'projects'
 
 urlpatterns = [
+    path('<slug:slug>-<int:pk>/<str:username>', views.ProjectDetailUser.as_view(), name='detail_user'),
     path('<slug:slug>-<int:pk>/', views.ProjectDetail.as_view(), name='detail'),
     path('', views.ProjectList.as_view(), name='list'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
 
+from account.models import User
 from .models import Project
 
 
@@ -22,3 +23,24 @@ class ProjectDetail(DetailView):
             pk=self.kwargs.get('pk'),
             slug=self.kwargs.get('slug')
         )
+
+
+class ProjectDetailUser(DetailView):
+    queryset = Project.objects.all()
+    template_name = 'projects/project_detail_user.html'
+
+    def get_object(self):
+        """
+        We need to find object by using `pk` and `slug`.
+        """
+        return get_object_or_404(
+            Project,
+            pk=self.kwargs.get('pk'),
+            slug=self.kwargs.get('slug')
+        )
+
+    def get_context_data(self, **kwargs):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        data = super().get_context_data(**kwargs)
+        data['project_user'] = user
+        return data
