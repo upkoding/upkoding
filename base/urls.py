7e95b0daac1685257c129a6a3fabdb440caf1a68
Eka Putra <ekaputra07@gmail.com>
2021-03-15 22:17:39 +1100

    Updated homepage

--- base/urls.py
+++ base/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
-from .views import render_template
+from .views import render_template, Index
 
 app_name = 'base'
 
 
 urlpatterns = [
-    path('', render_template('base/index.html'), name='index'),
+    path('', Index.as_view(), name='index'),
     path('diskusi/', render_template('base/forum.html'), name='forum'),
     path('tentang/', render_template('base/tentang.html'), name='tentang'),
     path('robots.txt', render_template('base/robots.txt', 'text/plain')),
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
+
+from projects.models import Project, UserProject
 
 
 def render_template(name, content_type="text/html; charset=utf-8"):
@@ -7,3 +8,20 @@ def render_template(name, content_type="text/html; charset=utf-8"):
     Just a shortcuts method.
     """
     return TemplateView.as_view(template_name=name, content_type=content_type)
+
+
+class Index(TemplateView):
+    template_name = 'base/index.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['projects'] = Project.objects.active()[:3]
+        data['user_projects_inprogress'] = UserProject.objects.filter(
+            status__in=(UserProject.STATUS_IN_PROGRESS,
+                        UserProject.STATUS_PENDING_REVIEW,
+                        UserProject.STATUS_INCOMPLETE)
+        ).order_by('-updated')[:5]
+        data['user_projects_completed'] = UserProject.objects.filter(
+            status=UserProject.STATUS_COMPLETE
+        ).order_by('-updated')[:5]
+        return data
