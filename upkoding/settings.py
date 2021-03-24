520bb7050775554b913665570df55dc10953b4e1
Eka Putra <ekaputra07@gmail.com>
2021-03-25 09:15:30 +1100

    Added S3 custom domain to settings

--- upkoding/settings.py
+++ upkoding/settings.py
@@ -163,6 +163,7 @@ if not DEBUG:
     AWS_S3_ENDPOINT_URL = 'https://{}.digitaloceanspaces.com'.format(
         AWS_S3_REGION_NAME
     )
+    AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
