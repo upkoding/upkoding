3ccfb33d1529ac829b955c629fc24bc265e56931
Eka Putra <ekaputra07@gmail.com>
2021-08-27 07:19:17 +1000

    feat: Added Midtrans payment notification model

--- account/models.py
+++ account/models.py
@@ -1,4 +1,6 @@
-from django.db.models import manager
+import uuid
+from datetime import timedelta
+from django.utils.timezone import now
 from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -119,3 +121,110 @@ class UserSetting(models.Model):
 
     def __str__(self):
         return '[{}] {}:{}'.format(self.user.pk, self.key, self.value)
+
+
+class ProAccess(models.Model):
+    """
+    To keep track of user's PRO access lifetime.
+    """
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='pro_access')
+    start = models.DateTimeField(auto_now_add=True)
+    end = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+def valid_until_time():
+    """
+    Once user purchase an access, the purchase will be valid for limited amount of time.
+    Once invalid, user will unable to make payment on that specific purchase and need to create the new one.
+    """
+    return now() + timedelta(days=3)
+
+
+class ProAccessPurchase(models.Model):
+    """
+    User could purchase an access or extend their access lifetime anytime.
+    A purchase valid for limited amount of time eg. 3 days and they need to
+    make payment during this time.
+    """
+    DAYS_30 = 30
+    DAYS_90 = 90
+    DAYS_365 = 365
+    DAYS_CHOICES = [
+        (DAYS_30, '1 bulan (30 hari)'),
+        (DAYS_90, '3 bulan (90 hari)'),
+        (DAYS_365, '1 tahun (365 hari)'),
+    ]
+
+    STATUS_PENDING = 0
+    STATUS_PAID = 1
+    STATUS_CANCELED = 2
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='purchases')
+    pro_access = models.ForeignKey(
+        ProAccess, on_delete=models.CASCADE, related_name='purchases')
+    days = models.IntegerField(choices=DAYS_CHOICES)
+    price = models.IntegerField()
+    valid_until = models.DateTimeField(default=valid_until_time)
+    paid_at = models.DateTimeField(blank=True, null=True)
+    gifted_at = models.DateTimeField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    @staticmethod
+    def safe_get(id: str):
+        try:
+            return ProAccessPurchase.objects.get(pk=id)
+        except ObjectDoesNotExist:
+            return None
+
+
+class MidtransPaymentNotification(models.Model):
+    """
+    Store history of payment notification received from Midtrans.
+    https://docs.midtrans.com/en/after-payment/http-notification
+    """
+    # a reference to ProAccessPurchase, allow null in case order_id (ProAccessPurchase) not found.
+    purchase = models.ForeignKey(
+        ProAccessPurchase,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True)
+    # original raw JSON payload from notification
+    payload = models.JSONField(blank=True, null=True)
+    payment_type = models.CharField()
+    transaction_id = models.CharField()
+    transaction_status = models.CharField()
+    fraud_status = models.CharField(blank=True, null=True)
+    gross_amount = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    @staticmethod
+    def create_from_payload(payload: dict):
+        """
+        Only use this method on verified and trusted payload.
+        """
+        payment_type = payload.get('payment_type')
+        transaction_id = payload.get('transaction_id')
+        transaction_status = payload.get('transaction_status')
+        fraud_status = payload.get('fraud_status')
+        gross_amount = float(payload.get('gross_amount', '0'))
+
+        order_id = payload.get('order_id')
+        purchase = ProAccessPurchase.get_by_id_safe(order_id)
+
+        payment_notification = MidtransPaymentNotification(
+            purchase=purchase,
+            payment_type=payment_type,
+            transaction_id=transaction_id,
+            transaction_status=transaction_status,
+            fraud_status=fraud_status,
+            gross_amount=gross_amount,
+            payload=payload
+        )
+        payment_notification.save()
