e44d3b5a7f742db9c3aed4cddb6a857230d22a0d
Eka Putra <ekaputra07@gmail.com>
2021-10-14 08:34:47 +1100

    feat: finished discord user verifivation.

--- discord/interactions.py
+++ discord/interactions.py
@@ -9,6 +9,20 @@ from discord.conf import API_BASE_URL, GUILD_ID, UPKODERS_ROLE_ID, BOT_REQUEST_H
 log = logging.getLogger(__name__)
 
 
+def set_user_role(user_id, role_id):
+    set_role_url = f'{API_BASE_URL}/guilds/{GUILD_ID}/members/{user_id}/roles/{role_id}'
+    resp = requests.put(url=set_role_url, headers=BOT_REQUEST_HEADERS)
+    resp.raise_for_status()
+    return resp
+
+
+def delete_message(channel_id, message_id):
+    delete_message_url = f'{API_BASE_URL}/channels/{channel_id}/messages/{message_id}'
+    resp = requests.delete(url=delete_message_url, headers=BOT_REQUEST_HEADERS)
+    resp.raise_for_status()
+    return resp
+
+
 def verifikasi(command, data):
     """
     Verify user's Discord token.
@@ -16,9 +30,11 @@ def verifikasi(command, data):
     """
     discord_user = data.get('user')
     discord_member = data.get('member')
-    # if interaction made from channel, throw error.
+
+    # if interaction made from channel, delete the message and throw error.
     if discord_member:
-        raise Exception('Jangan verifikasi akun di channel, DM saya langsung!')
+        raise Exception(
+            'Jangan verifikasi akun di channel, DM saya langsung! (Token verifikasi adalah rahasia, tolong hapus pesan ini).')
 
     # read command options
     options = command.get('options')
@@ -49,9 +65,7 @@ def verifikasi(command, data):
 
         # upgrade roles
         discord_user_id = discord_user.get('id')
-        set_role_url = f'{API_BASE_URL}/guilds/{GUILD_ID}/members/{discord_user_id}/roles/{UPKODERS_ROLE_ID}'
-        resp = requests.put(url=set_role_url, headers=BOT_REQUEST_HEADERS)
-        resp.raise_for_status()
+        set_user_role(discord_user_id, UPKODERS_ROLE_ID)
 
         # update token status
         verified_status = {
