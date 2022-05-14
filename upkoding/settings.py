f3d31bbe06a18be7c48339e616781e0e1d7e7f28
Eka Putra <ekaputra07@gmail.com>
2022-05-15 01:59:53 +1000

    Update settings.py

--- upkoding/settings.py
+++ upkoding/settings.py
@@ -191,8 +191,7 @@ DISCORD_UPKODERS_ROLE_ID = os.getenv("DISCORD_UPKODERS_ROLE_ID")
 
 if not DEBUG:
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
-    AWS_S3_ENDPOINT_URL = "https://{}.digitaloceanspaces.com".format(AWS_S3_REGION_NAME)
+    AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
     AWS_S3_CUSTOM_DOMAIN = os.getenv("AWS_S3_CUSTOM_DOMAIN")
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
